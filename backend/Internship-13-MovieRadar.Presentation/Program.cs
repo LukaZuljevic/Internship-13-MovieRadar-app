4bc826f4b77beaf3f0a261e9fd81f5b0f1c6002e
zulja1 <luka.zuljo@gmail.com>
2025-02-10 17:23:03 +0100

    Change data layer structure

--- backend/Internship-13-MovieRadar.Presentation/Program.cs
+++ backend/Internship-13-MovieRadar.Presentation/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Internship_13_MovieRadar.Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IConfiguration>(new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build())
+            .AddSingleton<DbConnectionFactory>()
+            .BuildServiceProvider();
+
+        var dbConnectionFactory = serviceProvider.GetService<DbConnectionFactory>();
+
+        try
+        {
+            using var connection = dbConnectionFactory.CreateConnection();
+            connection.Open();
+            Console.WriteLine("Database Connection Successful!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($" Database Connection Failed: {ex.Message}");
+        }
+    }
+}
