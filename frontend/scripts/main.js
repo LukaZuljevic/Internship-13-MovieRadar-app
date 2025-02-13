b2d27496e080e0fc9fc8cb77596deef44fa7708a
ticinovicsara <ticinovicsara0@gmail.com>
2025-02-13 21:25:48 +0100

    main.js change

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,5 +1,17 @@
 import { setupLoginRegister } from "./login-registration/login.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  setupLoginRegister();
+  const token = getCookie("secretKey");
+  if (token) {
+    window.location.href = "landingPage.html";
+  } else {
+    setupLoginRegister();
+  }
 });
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+  return null;
+}
